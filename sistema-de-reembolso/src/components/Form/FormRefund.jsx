64bdad7441030168f8a39540d77c1a352e16baad
Gabriel Duarte <biel.dbastos@gmail.com>
2022-08-18 00:59:28 -0300

    Atualizar funcionando

--- sistema-de-reembolso/src/components/Form/FormRefund.jsx
+++ sistema-de-reembolso/src/components/Form/FormRefund.jsx
@@ -29,9 +29,10 @@ import { handleForm } from "../../store/actions/formActions";
 const FormRefund = ({ dispatch, disabled, refundById, isLoading }) => {
   const navigate = useNavigate();
   const { idRefund } = useParams();
-  const [selectedFile, setSelectedFile] = useState("");
+  const [selectedFile, setSelectedFile] = useState("Nenhum anexo selecionado");
 
   const handleFile = (file, setFieldValue) => {
+    console.log(file)
     setFieldValue("file", file);
     setSelectedFile(file.name);
   };
@@ -39,9 +40,13 @@ const FormRefund = ({ dispatch, disabled, refundById, isLoading }) => {
   useEffect(() => {
     if (idRefund) {
       getRefundById(dispatch, idRefund);
-    }
+    } 
   }, []);
-
+  
+  useEffect(() => {
+    refundById.anexoDTO && setSelectedFile(refundById.anexoDTO.nome)
+  }, [refundById.anexoDTO])
+  
   if (isLoading) {
     return <Loading />;
   }
@@ -53,11 +58,11 @@ const FormRefund = ({ dispatch, disabled, refundById, isLoading }) => {
           <img src={logoAzul} alt="Logo DBC" />
           <h1>{idRefund ? "Atualizar" : "Solicitar"} reembolso</h1>
         </HeaderForm>
-        <Formik
+        <Formik 
           initialValues={{
-            titulo: refundById.titulo || "",
+            titulo: refundById ? refundById.titulo : '',
             valor: refundById.valor || "",
-            file: "",
+            file: refundById.anexoDTO ? refundById.anexoDTO.file : '',
           }}
           validationSchema={validationRefund}
           onSubmit={(values) => {
@@ -76,7 +81,6 @@ const FormRefund = ({ dispatch, disabled, refundById, isLoading }) => {
         >
           {({ errors, touched, handleSubmit, values, setFieldValue }) => (
             <FieldForm onSubmit={handleSubmit} encType="multipart/form-data">
-              {setSelectedFile(refundById.anexoDTO && refundById.anexoDTO.nome)}
 
               <FormItem>
                 <label htmlFor="titulo">título*</label>
@@ -122,7 +126,7 @@ const FormRefund = ({ dispatch, disabled, refundById, isLoading }) => {
                     }
                   />
 
-                  <small>{selectedFile || "Nenhum anexo selecionado"}</small>
+                  <small>{selectedFile}</small>
 
                   <button
                     type="button"
